42f737e5ceb9552bb3ebefbde3a95966c214aab3
Philip Simonson <drunkenmaster1988@gmail.com>
2018-02-12 21:44:24 -0500

    Updated test7 to include line numbers where the first occurrence of a syntax error is.

--- misc/test7.c
+++ misc/test7.c
@@ -8,53 +8,68 @@ const char key[] = "298374dkfjlsdjfsdkjf893uFKJDFJF";
 
 int is_match(int left, int right)
 {
-	return ((left == '<' && right == '>') ||
-			(left == '[' && right == ']') ||
+	return ((left == '{' && right == '}') ||
 			(left == '(' && right == ')') ||
-			(left == '{' && right == '}'));
+			(left == '[' && right == ']') ||
+			(left == '<' && right == '>'));
 }
 
 /* program to test c source files for minor syntax errors */
 int main(int argc, char *argv[])
 {
 	stack_t *stack;
+	stack_t *stack_ln;
 	FILE *file;
 	int syntax_errors;
+	int linenum;
 	int c;
 
 	stack = stack_create(8);
+	stack_ln = stack_create(8);
 	if (stack == NULL)
 		return -1;
+	if (stack_ln == NULL) {
+		stack_destroy(stack);
+		return -1;
+	}
 	do_log(write_log, LOG_NAME, "Stack created!\n");
 	if ((file = fopen(TEST_FILE, "rt")) == NULL) {
 		do_log(append_log, LOG_NAME, "Error: Invalid file or cannot open.\n");
 		stack_destroy(stack);
 		return -1;
 	}
-	syntax_errors = 0;
+	syntax_errors = linenum = 0;
 	while ((c = fgetc(file)) != EOF) {
 		switch (c) {
+		case '\n':
+		case '\r':
+			++linenum;
+		break;
 		case '{':
 		case '(':
 		case '[':
 		case '<':
 			stack_pushback(stack, c, -1);
+			stack_pushback(stack_ln, linenum, -1);
 		break;
 		}
 		if (c == '>' || c == ']' || c == ')' || c == '}') {
 			int left;
+			int left_linenum;
 			left = stack_popback(stack);
+			left_linenum = stack_popback(stack_ln);
 			if (!is_match(left, c)) {
-				do_log(append_log, LOG_NAME, "Syntax error : %c doesn't match %c\n",
-					left, c, is_match(left, c));
-				fprintf(stderr, "Syntax error : %c doesn't match %c\n",
-					left, c);
+				do_log(append_log, LOG_NAME, "Syntax error : %c doesn't match"
+					" %c : line %d\n", left, c, left_linenum);
+				fprintf(stderr, "Syntax error : %c doesn't match %c : line %d\n",
+					left, c, left_linenum);
 				syntax_errors += 1;
 			}
 		}
 	}
 	fclose(file);
 	stack_destroy(stack);
+	stack_destroy(stack_ln);
 	do_log(append_log, LOG_NAME, "File processed... results below.\n"
 		"Syntax errors : %d\n", syntax_errors);
 	fprintf(stderr, "File processed... results below.\n"
