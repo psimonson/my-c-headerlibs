63e769e107ec2df0bd965cab1cc63ed520c6e95f
Philip Simonson <drunkenmaster1988@gmail.com>
2018-02-13 00:31:15 -0500

    Total rewrite of test7.c under misc (But with a bug)

--- misc/test7.c
+++ misc/test7.c
@@ -6,79 +6,119 @@
 
 const char key[] = "298374dkfjlsdjfsdkjf893uFKJDFJF";
 
+enum {
+	CODE,
+	COMMENT,
+	QUOTE1,
+	QUOTE2
+};
+
 int is_match(int left, int right)
 {
-	return ((left == '{' && right == '}') ||
-			(left == '(' && right == ')') ||
-			(left == '[' && right == ']') ||
-			(left == '<' && right == '>'));
+	return ((left == '}' && right == '{') ||
+			(left == ')' && right == '(') ||
+			(left == ']' && right == '['));
 }
 
 /* program to test c source files for minor syntax errors */
 int main(int argc, char *argv[])
 {
 	stack_t *stack;
-	stack_t *stack_ln;
 	FILE *file;
 	int syntax_errors;
-	int linenum;
+	int state;
+	int error;
+	size_t linenum;
 	int c;
 
 	stack = stack_create(8);
-	stack_ln = stack_create(8);
 	if (stack == NULL)
 		return -1;
-	if (stack_ln == NULL) {
-		stack_destroy(stack);
-		return -1;
-	}
 	do_log(write_log, LOG_NAME, "Stack created!\n");
 	if ((file = fopen(TEST_FILE, "rt")) == NULL) {
 		do_log(append_log, LOG_NAME, "Error: Invalid file or cannot open.\n");
 		stack_destroy(stack);
 		return -1;
 	}
-	syntax_errors = linenum = 0;
+	state = CODE;
+	syntax_errors = error = linenum = 0;
 	while ((c = fgetc(file)) != EOF) {
-		switch (c) {
-		case '\n':
-		case '\r':
-			++linenum;
-		break;
-		case '{':
-		case '(':
-		case '[':
-		case '<':
-			stack_pushback(stack, c, -1);
-			stack_pushback(stack_ln, linenum, -1);
-		break;
+		if (c == '\n' || c == '\r') {
+			linenum++;
 		}
-		if (c == '>' || c == ']' || c == ')' || c == '}') {
-			int left_linenum;
-			int left;
-			left = stack_popback(stack);
-			left_linenum = stack_popback(stack_ln);
-			if (!is_match(left, c)) {
-				do_log(append_log, LOG_NAME, "Syntax error : %c doesn't match"
-					" %c : line %d\n", left, c, left_linenum);
-				fprintf(stderr, "Syntax error : %c doesn't match %c : line %d\n",
-					left, c, left_linenum);
-				syntax_errors += 1;
+
+		switch (state) {
+		case CODE:
+			if (c == '\'') {
+				state = QUOTE1;
+			} else if (c == '"') {
+				state = QUOTE2;
+			} else if (c == '/') {
+				int second = fgetc(file);
+				if (second == '*') {
+					state = COMMENT;
+				} else {
+					ungetc(second, file);
+				}
+			} else if (c == '(' || c == '[' || c == '{') {
+				stack_pushback(stack, c, -1);
+			} else if (c == '}' || c == ']' || c == ')') {
+				char open = stack_popback(stack);
+				if (!is_match(c, open)) {
+					printf("Line %lu: Closing '%c' missing opening '%c'.\n",
+						(unsigned long)linenum, c, open);
+					do_log(append_log, LOG_NAME,
+						"Line %lu: Closing '%c' missing opening '%c'.\n",
+						(unsigned long)linenum, c, open);
+					error = 1;
+					syntax_errors += 1;
+				}
 			}
+		break;
+		case COMMENT:
+			if (c == '*') {
+				int second = fgetc(file);
+				if (second == '/') {
+					state = CODE;
+				} else {
+					ungetc(second, file);
+				}
+			}
+		break;
+		case QUOTE1:
+			if (c == '\\') {
+				(void)fgetc(file); /* an escaped char */
+			} else {
+				state = CODE;
+			}
+		break;
+		case QUOTE2:
+			if (c == '\\') {
+				(void)fgetc(file); /* an escaped char */
+			} else {
+				state = CODE;
+			}
+		break;
+		default:
+		break;
 		}
 	}
 	fclose(file);
-	stack_destroy(stack);
-	stack_destroy(stack_ln);
-	if (syntax_errors > 0) {
-		do_log(append_log, LOG_NAME, "File processed..."
-			" syntax errors: %d\n", syntax_errors);
-		fprintf(stderr, "File processed..."
-			" syntax errors: %d\n", syntax_errors);
-	} else {
-		do_log(append_log, LOG_NAME, "File processed... no syntax errors.\n");
-		fprintf(stderr, "File processed... no syntax errors.\n");
+	if (state == COMMENT) {
+		printf("Code ends inside a comment!\n");
+		do_log(append_log, LOG_NAME, "Code ends inside a comment!\n");
+	} else if (state == QUOTE1) {
+		printf("Code ends inside a single quote!\n");
+		do_log(append_log, LOG_NAME, "Code ends inside a single quote!\n");
+	} else if (state == QUOTE2) {
+		printf("Code ends inside a double quote!\n");
+		do_log(append_log, LOG_NAME, "Code ends inside a double quote!\n");
 	}
+	do_log(append_log, LOG_NAME, "File processed..."
+		" syntax errors: %d\n", syntax_errors);
+	fprintf(stderr, "File processed..."
+		" syntax errors: %d\n", syntax_errors);
+	stack_destroy(stack);
 	do_log(append_log, LOG_NAME, "Stack destroyed.\n");
 	return 0;
 }
