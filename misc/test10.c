42b1e18908116cf1bfc6c32fe70acbb15f019e04
Philip Simonson <drunkenmaster1988@gmail.com>
2018-02-18 12:54:34 -0500

    Commit changed from using stack to queue.

--- misc/test10.c
+++ misc/test10.c
@@ -1,6 +1,5 @@
-#include "../common/stack.h"
+#include "../common/queue.h"
 
-#define MAXVAL	1024
 #define MAXLINE	1024
 
 enum STATE {
@@ -12,7 +11,7 @@ typedef enum STATE state_t;
 
 /* global variables */
 char line[MAXLINE];			/* global line variable */
-stack_t stack;				/* stack variable */
+queue_t queue;				/* queue variable */
 state_t state;				/* state variable */
 int comments;				/* comment count */
 int quotes;					/* quotes count */
@@ -32,9 +31,9 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	if (argc == 1)
+	if (argc == 1) {
 		check_source(stdin);
-	else if (argc == 3) {
+	} else {
 		if (*argv[1] == '-') {
 			while (*++argv[1]) {
 				switch (*argv[1]) {
@@ -73,7 +72,8 @@ int getln(FILE *file)
 /* is_match:  checks for matching chars */
 int is_match(int c1, int c2)
 {
-	return ((c1 == '(' && c2 != ')') ||
+	return ((c1 == '<' && c2 != '>') ||
+			(c1 == '(' && c2 != ')') ||
 			(c1 == '[' && c2 != ']') ||
 			(c1 == '{' && c2 != '}'));
 }
@@ -81,27 +81,26 @@ int is_match(int c1, int c2)
 /* special_body:  prints output for syntax checker */
 void report(void)
 {
-	extern stack_t stack;
+	extern queue_t queue;
 	extern state_t state;
 	extern int comments;
 	extern int quotes;
 	extern int error;
 
-	if (comments > 0 || quotes > 0)
-		printf("Comments: %d\nQuotes: %d\n", comments, quotes);
 	if (state == COMMENT) {
 		printf("Code ends inside of comment.\n");
 		error = 1;
 	} else if (state == QUOTES) {
 		printf("Code ends inside of quotes.\n");
 		error = 1;
-	} else if (stack_is_empty(&stack) && error == 0) {
+	} else if (queue_is_empty(&queue) && error == 0) {
 		printf("Code seems to be okay.\n");
+		printf("Comments: %d\nQuotes: %d\n", comments, quotes);
 	} else {
-		while (!stack_is_empty(&stack)) {
+		while (!queue_is_empty(&queue)) {
 			int val, pos;
-			stack_pop(&stack, &val, &pos);
-			printf("Syntax error: line %d : '%c' missing counterpart\n",
+			queue_get(&queue, &val, &pos);
+			printf("Syntax error line %d : '%c' missing counterpart.\n",
 				pos, val);
 		}
 		printf("There were errors in the code.\n");
@@ -112,7 +111,7 @@ void report(void)
 int check_source(FILE *file)
 {
 	extern char line[];
-	extern stack_t stack;
+	extern queue_t queue;
 	extern state_t state;
 	extern int comments;
 	extern int quotes;
@@ -140,19 +139,19 @@ int check_source(FILE *file)
 						break;
 					}
 				}
-				if (c == '(' || c == '[' || c == '{') {
-					stack_push(&stack, c, ln);
-				} else if (c == ')' || c == ']' || c == '}') {
-					if (stack_is_empty(&stack)) {
-						printf("Syntax error: '%c' on line %d missing"
-							" counterpart.\n", c, ln);
+				if (c == '<' || c == '(' || c == '[' || c == '{') {
+					queue_add(&queue, c, ln);
+				} else if (c == '>' || c == ')' || c == ']' || c == '}') {
+					if (queue_is_empty(&queue)) {
+						printf("Syntax error line %d : '%c' missing"
+							" counterpart.\n", ln, c);
 						error = 1;
 					} else {
 						int val, pos;
-						stack_pop(&stack, &val, &pos);
+						queue_get(&queue, &val, &pos);
 						if (is_match(c, val)) {
-							printf("Syntax error: '%c' on line %d missing"
-								" '%c'.\n", val, pos, c);
+							printf("Syntax error line %d : '%c' missing"
+								" '%c'.\n", pos, val, c);
 							error = 1;
 						}
 					}
