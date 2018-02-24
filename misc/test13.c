da93a04c2da3e5173a9a88fd8cc9d0fca1650560
Philip Simonson <drunkenmaster1988@gmail.com>
2018-02-24 18:34:32 -0500

    Made test13.c into a full test (it tests if directory/file exists

--- misc/test13.c
+++ misc/test13.c
@@ -1,6 +1,11 @@
 #include "../common/prs_string.h"
 #include <string.h>
 #include <dirent.h>
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
 
 #define MAXDIR 1024
 
@@ -9,15 +14,33 @@ char dir[MAXDIR];
 /* program to test my find function */
 int main(int argc, char *argv[])
 {
-	int find(void);
+	int find_directory(void);
+	int find_file(void);
 
-	return find();
+	if (argc > 2) {
+		printf("Usage: %s [-f]\n", argv[0]);
+		return 1;
+	}
+
+	if (argc == 1)
+		return find_directory();
+	else if (argc == 2 && argv[1][0] == '-') {
+		while(*++argv[1]) {
+			switch (*argv[1]) {
+			case 'f':
+				return find_file();
+			break;
+			default:
+				printf("Unknown option '%c'.\n", *argv[1]);
+			}
+		}
+	}
+	return 2;
 }
 
 /* find:  allows to append to end of current directory */
-int find(void)
+int find_directory(void)
 {
-	extern int getcwd(char *s, size_t size);
 	char buf[BUFSIZ];
 	DIR *d;
 
@@ -34,5 +57,30 @@ int find(void)
 	}
 	closedir(d);
 	printf("Directory exists!\n");
+	return 0;
+}
+
+int find_file(void)
+{
+	char buf[BUFSIZ];
+
+	get_input("Enter a filename: ", buf, BUFSIZ);
+	getcwd(dir, MAXDIR);
+	strncat(dir, "/", 1);
+	strncat(dir, buf, strlen(buf));
+	printf("You entered: %s\nCurrent Filename: %s\n",
+		buf, dir);
+#ifdef _WIN32
+	if (_access(dir, 0) < 0) {
+		printf("File doesn't exist.\n");
+		return 1;
+	}
+#else
+	if (access(dir, 0) < 0) {
+		printf("File doesn't exist.\n");
+		return 1;
+	}
+#endif
+	printf("File exists.\n");
 	return 0;
 }
