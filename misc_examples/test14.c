1eeebaca6d9bb44300cfcef9e390574a157d0e6d
Philip Simonson <drunkenmaster1988@gmail.com>
2018-04-25 20:29:45 +0000

    Added new example for dlist.h (this one will override default functions)

---
+++ misc_examples/test14.c
@@ -0,0 +1,31 @@
+struct dlist {
+	char *data;
+	int id;
+	struct dlist *next;
+};
+#define DLIST_DATA_OPTIONS
+#include "../common/dlist.h"
+
+/* program to test my dynamic linked list */
+int main()
+{
+	struct dlist_data *data;
+
+	/* list create, set, add new node */
+	data = dlist_init(NULL, NULL, NULL);
+	data->list = dlist_create("Hello.");
+	data->add(data->list, "Hello world");
+	data->add(data->list, "Hello again world!");
+
+	/* now display the list */
+	dlist_display(data->list);
+	/* display the count */
+	printf("List item count: %d\n", dlist_get_count(data->list));
+
+	/* destroy the list */
+	data->destroy(&data->list);
+	printf("List empty status: %s\n",
+		(dlist_is_empty(data->list) == 0) ? "Empty" : "Not Empty");
+	dlist_cleanup(data);
+	return EXIT_SUCCESS; /* return success */
+}
