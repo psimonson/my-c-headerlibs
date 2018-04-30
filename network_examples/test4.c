4ffdc3c8468dfe31e2d24f276a2c8313ad9d05f2
Philip Simonson <drunkenmaster1988@gmail.com>
2018-04-30 23:21:17 +0000

    Fixed issues in test4.c, added a client example network_examples/test5.c

--- network_examples/test4.c
+++ network_examples/test4.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	SERVER_INFO;	/* contains sockfd,newfd */
-	char msg[256];
 	char address[INET6_ADDRSTRLEN];
+	char msg[256];
 	int port;
 	
 	sockfd = create_server(0, 5000, "0.0.0.0");
@@ -13,12 +13,14 @@ int main(void)
 	newfd = accept_conn(sockfd);
 	if (newfd == BAD_SOCKET)
 		return 1;
-
+	
 	get_addr_info(newfd, address, &port);
-	sprintf(msg, "%s:%d connected!\n", address, port);
+	sprintf(msg, "%s:%d connected!", address, port);
 	puts(msg);
 	send_msg(newfd, "Welcome to sock help library.\r\n");
 	close_conn(newfd);
+	sprintf(msg, "%s:%d disconnected!", address, port);
+	puts(msg);
 	close_conn(sockfd);
 	return 0;
 }
---
+++ network_examples/test5.c
@@ -0,0 +1,39 @@
+#include "../common/sock_help.h"
+
+int main(int argc, char *argv[])
+{
+	CLIENT_INFO;
+	char msg[256];
+	char address[INET6_ADDRSTRLEN];
+	int port, bytes;
+
+	if (argc != 1 && argc != 3) {
+		printf("Usage: %s <address> <port>\n", argv[0]);
+		return 2;
+	}
+	if (argc == 3)
+		sockfd = create_client(0, atoi(argv[2]), argv[1]);
+	else
+		sockfd = create_client(0, 5000, "127.0.0.1");
+	if (sockfd == BAD_SOCKET)
+		return 1;
+
+	/* do something with server */
+	get_addr_info(sockfd, address, &port);
+	sprintf(msg, "Connected to %s on port %d.", address, port);
+	puts(msg);
+
+	mem_set(msg, 0, sizeof msg);
+	/* recv message from server */
+	while ((bytes = recv(sockfd, msg, sizeof msg, 0)) > 0) {
+		printf("%s", msg);
+		mem_set(msg, 0, sizeof msg);
+	}
+	mem_set(msg, 0, sizeof msg);
+	if (bytes == 0)
+		printf("Disconnected from %s\n", address);
+	else
+		printf("Error receiving from %s\n", address);
+	close_conn(sockfd);
+	return 0;
+}
