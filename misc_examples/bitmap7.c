60c3f9b610a802de1388b7aa86921493f170e28e
Philip Simonson <drunkenmaster1988@gmail.com>
2018-05-07 04:33:27 -0400

    Added vertical line drawing function to bitmap header

--- misc_examples/bitmap7.c
+++ misc_examples/bitmap7.c
@@ -3,14 +3,16 @@
 int main(void)
 {
 	BITMAP_FILE *bmp;
-	int y;
+	int x,y;
 
 	bmp = create_BMP("test.bmp", 64, 64, 24);
 	if (!bmp)
 		return 1;
 	clear_BMP(bmp, 0xff, 0xff, 0xff);
 	for (y=bmp->header.info.height-1; y >= 0; y-=10)
-		draw_line_BMP(bmp, 1, 64, y, 255, 0, 0);
+		draw_hline_BMP(bmp, 1, 64, y, 255, 0, 0);
+	for (x=0; x <= bmp->header.info.width; x+=10)
+		draw_vline_BMP(bmp, x, 0, 64, 0, 255, 0);
 	write_BMP(bmp, 0);
 	display_info_BMP(bmp);
 	destroy_BMP(bmp);
