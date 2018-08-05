92c360a46919d472d0b8dfa3563209ed8261355a
Philip Ruben Simonson <drunkenmaster1988@gmail.com>
2018-08-05 18:57:05 -0400

    Added my new dynamic linked list header library (common/d_list.h)

---
+++ misc_examples/d_list.c
@@ -0,0 +1,19 @@
+#include "../common/d_list.h"
+#include <stdio.h>
+
+int main()
+{
+	struct DLIST *list, *tmp;
+	dlist_init(NULL, NULL, NULL, NULL);
+	list = dlist_create_list(1, "Philip");
+	dlist_append_node(&list, 2, "Mark");
+	dlist_append_node(&list, 3, "Bob");
+	dlist_iterator_init(list);
+	while ((tmp = dlist_iterator_next()) != NULL) {
+		printf("Hello, %s.\n", ((struct DLIST_DATA *)tmp->data)->message);
+	}
+	if (!tmp)
+		printf("No more data slots in list.\n");
+	dlist_destroy(&list);
+	return 0;
+}
